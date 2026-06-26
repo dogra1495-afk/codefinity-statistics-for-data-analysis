6ed81226b3328663cc8a6ffe2402f201735443e6
dogra1495-afk <>
2026-06-26 12:49:57 +0000

    No content

--- section/no_content/main.py
+++ section/no_content/main.py
@@ -1,12 +1,12 @@
-# Import library
+# Import library 
 import pandas as pd
 
 df = pd.read_csv('https://codefinity-content-media.s3.eu-west-1.amazonaws.com/a849660e-ddfa-4033-80a6-94a1b7772e23/update/ds_salaries_statistics', index_col = 0)
 
-# Calculating the mean value
-mean = df['salary'].mean()
-# Calculating the median value
-median = df['salary'].median()
+# Calculate the variance
+var = df['salary'].var()
+# Calculate the standard deviation 
+std = df['salary'].std()
 
-print('The mean value is', mean)
-print('The median value is', median)
+print('The variace is', round(var, 2))
+print('The standard deviation is', round(std, 2))
